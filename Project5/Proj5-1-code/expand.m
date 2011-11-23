4af0f892441f930765d386a42138b4369e8cf625
Daniel Gaviria <ggdaniel@gmail.com>
2011-11-23 12:38:41 -0500

    finished Part1

--- Project5/Proj5-1-code/expand.m
+++ Project5/Proj5-1-code/expand.m
@@ -10,10 +10,10 @@ for r = 4:newRows-2
     for c = 4:newCols-2
         for m= -2:2
             for n= -2:2
-%                 if isa((r-m)/2, 'integer') && isa((c-n)/2,'integer')
-%                     newIm(r,c) = newIm(r,c) + (w(m+3,n+3) * im((r-m)/2, (c-n)/2));
-%                 end
-                newIm(r,c) = newIm(r,c) + (w(m+3,n+3) * im(floor((r-m)/2), floor((c-n)/2)));
+                if (mod(r-m, 2)==0) && (mod(c-n, 2)==0)
+                    newIm(r,c) = newIm(r,c) + (w(m+3,n+3) * im((r-m)/2, (c-n)/2));
+                end
+%                 newIm(r,c) = newIm(r,c) + (w(m+3,n+3) * im(floor((r-m)/2), floor((c-n)/2)));
             end
         end
         newIm(r,c) = 4*newIm(r,c);
--- Project5/Proj5-1-code/script.m
+++ Project5/Proj5-1-code/script.m
@@ -4,30 +4,29 @@ levels = 4;
 
 
 g1 = reduce(g0);
-imwrite(uint8(newIm1),'Proj5-1-1-1.jpg');
+imwrite(uint8(g1),'Proj5-1-1-1.jpg');
 
 g2 = reduce(g1);
-imwrite(uint8(newIm2),'Proj5-1-1-2.jpg');
+imwrite(uint8(g2),'Proj5-1-1-2.jpg');
 
 g3 = reduce(g2);
-imwrite(uint8(newIm3),'Proj5-1-1-3.jpg');
+imwrite(uint8(g3),'Proj5-1-1-3.jpg');
 
 g4 = reduce(g3);
-imwrite(uint8(newIm4),'Proj5-1-1-4.jpg');
+imwrite(uint8(g4),'Proj5-1-1-4.jpg');
 
 %% Script for Code 2.2
 L4 = g4;
+imwrite(uint8(L4),'Proj5-1-2-1.jpg');
 
 L3 = g3 - expand(g4);
+imwrite(uint8(L3),'Proj5-1-2-2.jpg');
 
 L2 = g2 - expand(g3);
+imwrite(uint8(L2),'Proj5-1-2-3.jpg');
 
-imwrite(uint8(im3lap),'Proj5-1-2-1.jpg');
+L1 = g1 - expand(g2);
+imwrite(uint8(L1),'Proj5-1-2-4.jpg');
 
-newIm2ex = expand(newIm3);
-im2lap = newIm2ex - newIm2;
-imwrite(uint8(im2lap),'Proj5-1-2-2.jpg');
-
-newIm1ex = expand(newIm2);
-im1lap = newIm1ex - newIm1;
-imwrite(uint8(im1lap),'Proj5-1-2-3.jpg');
+L0 = g0 - expand(g1);
+imwrite(uint8(L0),'Proj5-1-2-5.jpg');
